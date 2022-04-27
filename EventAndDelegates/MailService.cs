98144feab57977f22dbb6c11fed45df77e443ba4
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-27 16:23:59 +0530

    Add files via upload

---
+++ EventAndDelegates/MailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventAndDelegates
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MailService : Sending Mail...." + e.Video.Title);
+        }
+    }
+
+}
